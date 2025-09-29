d78cba6c7bc878fd8a3a07d5f711762ad2b2cb04
Xelax-XIII <gabax.barsav@gmail.com>
2025-09-29 19:55:55 +0200

    début partie 4.4

---
+++ src/personnages/Druide.java
@@ -0,0 +1,45 @@
+package personnages;
+
+public class Druide {
+	private String nom;
+	private int force;
+	private Chaudron chaudron;
+	
+	public Druide(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+		this.chaudron = new Chaudron();
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void fabriquerPotion(int qte, int forcePotion) {
+		chaudron.remplirChaudron(qte, forcePotion);
+		parler("j'ai concocté " + chaudron.getQte() + " dose de portion magique. Elle a une force de " + chaudron.getForcePotion());
+	}
+	
+	public void boosterGaulois(Gaulois gaulois) {
+		String nomGaulois = gaulois.getNom();
+		if (chaudron.resterPotion()) {
+			if(nomGaulois == "Obelix") {
+				parler("ma belle tu peux pas");
+			}
+			else {
+				
+			}
+		}
+		else {
+			parler("dsl "+ nomGaulois +" j'ai plus potion bro");
+		}
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreparole() + "\"" + texte + "\"");
+	}
+
+	private String prendreparole() {
+		return "Le Druide " + nom + " : ";
+	}
+}
